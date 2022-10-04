c0d6278060a4991ba1a1fc0ddf6690ecc6ebf00a
RokLcw <lcwlyw@naver.com>
2022-10-04 15:49:30 +0900

    test2

---
+++ test2.py
@@ -0,0 +1,18 @@
+def 기능1():
+    print("기능1")
+
+def 기능2():
+    print("기능2")
+
+def 기능3():
+    print("기능3")
+
+def main():
+    print("main")
+
+    기능1()
+    기능2()
+    기능3()
+
+if __name__ == '__main__':
+    main()
