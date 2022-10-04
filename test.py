23816630ac74c764466bb758e21d688f25c00a25
RokLcw <lcwlyw@naver.com>
2022-10-04 16:27:51 +0900

    test.py

--- test.py
+++ test.py
@@ -5,9 +5,7 @@ print("i'm JeonginLee")
 
 print("오 좀 치네")
 
-<<<<<<< HEAD
-print("update by jeong0000")
-=======
-print("오주연")
 
->>>>>>> 5f7be877b0105ab7e4302c7b73a03877405c3d62
+print("update by jeong0000")
+
+print("오주연")
