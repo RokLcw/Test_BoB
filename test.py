109ea4839aaa0d18031d414559804b13dd8475a4
jeong0000 <vkdrk23@naver.com>
2022-10-04 16:36:02 +0900

    망함마함

--- test.py
+++ test.py
@@ -5,13 +5,25 @@ print("i'm JeonginLee")
 
 print("오 좀 치네")
 
-<<<<<<< HEAD
-print("update by jeong0000")
-=======
+
 print("오주연")
 
->>>>>>> 5f7be877b0105ab7e4302c7b73a03877405c3d62
+
+print("작업중입니다")
+
+print("작업중이에요~~~~")
 
 
+<<<<<<< HEAD
 print("create branch 'jeong'")
 print("test1")
+=======
+
+print("create branch 'jeong'")
+
+
+print("update by jeong0000")
+
+print("오주연")
+
+>>>>>>> 8f4bdbf75b591d7fe2781285ac9e31c3dba1fabb
