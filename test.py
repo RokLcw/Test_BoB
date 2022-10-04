38363226a9e4606f6dc0a07daf7ebabff26ba429
RokLcw <lcwlyw@naver.com>
2022-10-04 16:22:24 +0900

    수정

--- test.py
+++ test.py
@@ -5,9 +5,8 @@ print("i'm JeonginLee")
 
 print("오 좀 치네")
 
-<<<<<<< HEAD
-print("update by jeong0000")
-=======
 print("오주연")
 
->>>>>>> 5f7be877b0105ab7e4302c7b73a03877405c3d62
+print("작업중입니다")
+
+print("작업중이에요~~~~")
