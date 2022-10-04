332e6e2fda6003f628a79aaf1dd739d524465bd3
juyeonoh <ojy4493@naver.com>
2022-10-04 17:10:30 +0900

    testest

--- test.py
+++ test.py
@@ -5,13 +5,23 @@ print("i'm JeonginLee")
 
 print("오 좀 치네")
 
-<<<<<<< HEAD
-print("update by jeong0000")
-=======
+
 print("오주연")
 
-<<<<<<< HEAD
-print("hello")
-=======
->>>>>>> 5f7be877b0105ab7e4302c7b73a03877405c3d62
->>>>>>> a5b9d45236230497759d5a1a5010ceb622e25b13
+
+print("작업중입니다")
+
+print("작업중이에요~~~~")
+
+
+
+print("create branch 'jeong'")
+print("test1")
+
+
+print("create branch 'jeong'")
+
+
+print("update by jeong0000")
+
+print("오주연")
