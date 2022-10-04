d427dc7ec3dd18c08d88e5829c03b2e4717e2201
RokLcw <lcwlyw@naver.com>
2022-10-04 16:45:44 +0900

    바꾸지마라

--- test.py
+++ test.py
@@ -14,13 +14,10 @@ print("작업중입니다")
 print("작업중이에요~~~~")
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 print("create branch 'jeong'")
 print("test1")
-=======
-=======
->>>>>>> 8f4bdbf75b591d7fe2781285ac9e31c3dba1fabb
+
 
 print("create branch 'jeong'")
 
@@ -28,8 +25,3 @@ print("create branch 'jeong'")
 print("update by jeong0000")
 
 print("오주연")
-
-<<<<<<< HEAD
->>>>>>> 8f4bdbf75b591d7fe2781285ac9e31c3dba1fabb
-=======
->>>>>>> 8f4bdbf75b591d7fe2781285ac9e31c3dba1fabb
